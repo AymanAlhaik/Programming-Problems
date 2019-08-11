5424fee61d02341bb85203169aff0c97b1fb7cb3
Ayman Alhayk Sy <38873195+MaherHasann@users.noreply.github.com>
2019-08-11 04:39:03 +0300

    Add files via upload
    In a plane N points are given (N < 100,000). The coordinates of the points are integers (xi, yi). Write a program, which finds all horizontal and vertical lines, such that they split the plane into two parts, each containing an equal set of points (points lying on the line are note counted)

---
+++ Exercise1/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
