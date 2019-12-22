1bf8e268b947d87c045e3b1318ac4eee69d2efd8
Ayman Alhaik Sy <38873195+AymanAlhaik@users.noreply.github.com>
2019-12-22 20:36:20 +0300

    Update rotate_list_by_kElement.dart

--- rotate_list_by_kElement.dart
+++ rotate_list_by_kElement.dart
@@ -1,3 +1,12 @@
+
+/*
+  write a function that rotates a list by a given number of elements k
+  [1, 2, 3, 4] and k=2 --> [3, 4, 1, 2]
+                       k=3 --> [4, 1, 2, 3]
+                       k=4 --> [1, 2, 3, 4]
+ */
+
+
 main(List<String> args) {
   List<int> list = [1, 2, 3, 4, 5];
   rotateList(list, 0);
